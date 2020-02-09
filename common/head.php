1fadeef1a5cd46d0353025cc602f3a06682a9aa7
Futo Nakajima <futonakajima@FutonoMacbookAir.local>
2020-02-10 02:06:28 +0900

    [feat]共通化

---
+++ common/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="apple-mobile-web-app-title" content="部室予約">
+  <title>くされエン部室予約アプリ</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel=”icon” href=“icon/favicon.png”>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
--- index.php
+++ index.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <meta name="apple-mobile-web-app-title" content="部室予約">
-  <title>くされエン部室予約アプリ</title>
-  <link rel="stylesheet" href="css/style.css">
-  <link rel=”icon” href=“icon/favicon.png”>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-</head>
+<?php include 'common/head.php' ?>
 <body>
-  <header class="header">
-    <a class="header__link" href="/">
-      <h1 class="header__title">TestApp</h1>
-      <p class="header__description">くされエン部室予約アプリ</p>
-    </a>
-  </header>
+  <?php include 'common/header.php' ?>
   <section class="sub-header">
     <h2 class="sub-header__title">予約一覧</h2>
   </section>
