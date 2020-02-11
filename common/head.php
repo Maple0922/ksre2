2b8decdfd8e6021f8419e4e2de6164ff0e69eeec
Futo Nakajima <futonakajima@FutonoMacbookAir.local>
2020-02-11 11:49:29 +0900

    20200211

--- common/head.php
+++ common/head.php
@@ -6,6 +6,8 @@
   <meta name="apple-mobile-web-app-title" content="部室予約">
   <title>くされエン部室予約アプリ</title>
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/css/all.min.css">
   <link rel=”icon” href=“icon/favicon.png”>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/f8b0fcab1f.js" crossorigin="anonymous"></script>
 </head>
