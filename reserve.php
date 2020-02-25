4e7bfcfcd5d7ab57bce4e3d54166a9db1dfead84
Futo Nakajima <futonakajima@FutonoMacbookAir.local>
2020-02-25 16:10:22 +0900

    [fix] time data format

--- reserve.php
+++ reserve.php
@@ -7,10 +7,11 @@
   <main class="main">
     <div class="reserve">
       <form method="post" class="reserve__input" action="complete.php">
-        <label for="name" class="label-name"><img src="images/music.svg">バンド名</label>
+        <label class="label-name"><img src="images/music.svg">バンド名</label>
         <input class='form-name' type="text" required
-        placeholder="入力してください" name="name">
-        <label for="year" class="label-date"><img src="images/calendar.svg">予約日</label>
+        placeholder="入力してください" name="name"
+        value="ポルカドットスティングレイ">
+        <label class="label-date"><img src="images/calendar.svg">予約日</label>
         <select class="form-year" name="year" id="year">
           <?php
           for($i=0;$i<=1;$i++){
@@ -36,43 +37,41 @@
           ?>
         </select>
         <span>日</span>
-        <label for="startTime endTime" class="label-time"><img src="images/clock.svg">練習時間</label>
-        <select class='form-starttime' name='startTime' required>
-          <option value='' selected disabled>--:--</option>
+        <label class="label-time"><img src="images/clock.svg">練習時間</label>
+        <select class='form-starttime-hour' name='startTimeHour' required>
+          <!-- <option value='' selected disabled>−−</option> -->
           <?php
           for ($i=8;$i<=22;$i++) {
-            for ($j=0;$j<=30;$j+=30) {
-              $valueTime = $i*100+$j;
-              $textTime = "${i}:${j}";
-              if ($j==0) {
-                $textTime .= 0;
-              }
-              if ($valueTime>2200) {
-                continue;
-              }
-              echo "<option value='${valueTime}'>${textTime}</option>";
+            if($i<10){
+              echo "<option value='${i}'>${i}</option>";
+            }else{
+              echo "<option value='${i}'>${i}</option>";
             }
           }
           ?>
         </select>
-        <small>~</small>
-        <select class='form-endtime' name='endTime' required>
-          <option value='' selected disabled>--:--</option>
+        <small>:</small>
+        <select class='form-starttime-minute' name='startTimeMinute' required>
+          <option value='00' selected>00</option>
+          <option value='30'>30</option>
+        </select>
+        <small>  ~  </small>
+        <select class='form-endtime-hour' name='endTimeHour' required>
+          <!-- <option value='' selected disabled>−−</option> -->
           <?php
           for ($i=8;$i<=22;$i++) {
-            for ($j=0;$j<=30;$j+=30) {
-              $valueTime = $i*100+$j;
-              $textTime = "${i}:${j}";
-              if ($j==0) {
-                $textTime .= 0;
-              }
-              if ($valueTime>2200) {
-                continue;
-              }
-              echo "<option value='${valueTime}'>${textTime}</option>";
+            if($i<10){
+              echo "<option value='${i}'>${i}</option>";
+            }else{
+              echo "<option value='${i}'>${i}</option>";
             }
           }
           ?>
+        </select>
+        <small>:</small>
+        <select class='form-endtime-minute' name='endTimeMinute' required>
+          <option value='00' selected>00</option>
+          <option value='30'>30</option>
         </select>
         <br>
         <button class='button-primary' type="submit">予約</button>
